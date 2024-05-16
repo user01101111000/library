76c0e5678556a423a89f1f5acfa3aae09c578e27
S U R A Y  M I R Z E Y E V A <mirzayevasuray3@gmail.com>
2024-05-16 20:57:03 +0400

    search hisse son defe push edirem

--- src/tests/testSur/test.js
+++ src/tests/testSur/test.js
@@ -1,6 +1,6 @@
-// Yorum eklemek için bir fonksiyon
+
 function addComment(commentText, time) {
-    // Yeni bir yorum divi oluştur
+
     var newComment = document.createElement('div');
     newComment.className = 'yourComment';
     
@@ -15,21 +15,21 @@ function addComment(commentText, time) {
     headLine.appendChild(h4);
     headLine.appendChild(pTime);
     
-    // Yorum metni
+
     var pComment = document.createElement('p');
     pComment.className = 'comment';
     pComment.textContent = commentText;
     
-    // Başlık ve metni yeni yorum divine ekle
+ 
     newComment.appendChild(headLine);
     newComment.appendChild(pComment);
     
-    // Yeni yorumu sayfaya ekle
+
     var commentContainer = document.querySelector('.commentAnonymous');
     commentContainer.appendChild(newComment);
 }
 
-// Yeni yorum ekleme işlemi
+
 var sendIcon = document.querySelector('.sendIcon');
 sendIcon.addEventListener('click', function() {
     var inputField = document.querySelector('.commentObvious input');
